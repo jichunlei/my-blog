e33418402ea8bba7a6cc2913d958955f12ae3b85
jichunlei <1739575190@qq.com>
2019-11-25 18:42:13 +0800

    博客新增管理完成（50%）

--- src/main/java/com/jicl/web/admin/BlogController.java
+++ src/main/java/com/jicl/web/admin/BlogController.java
@@ -1,5 +1,6 @@
 package com.jicl.web.admin;
 
+import com.jicl.pojo.Blog;
 import com.jicl.service.BlogService;
 import com.jicl.service.TagService;
 import com.jicl.service.TypeService;
@@ -14,8 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.jws.WebParam;
-
 /**
  * @Auther: xianzilei
  * @Date: 2019/11/24 11:42
@@ -24,6 +23,10 @@ import javax.jws.WebParam;
 @Controller
 @RequestMapping("/admin")
 public class BlogController {
+
+    private static final String INPUT = "admin/blogs-input";
+    private static final String LIST = "admin/blogs";
+    private static final String REDIRECT_LIST = "redirect:/admin/blogs";
 
     @Autowired
     private BlogService blogService;
@@ -36,25 +39,25 @@ public class BlogController {
      * 根据条件分页查询博客列表
      *
      * @param pageable 1
-     * @param blog 2
-     * @param model 3
+     * @param blog     2
+     * @param model    3
      * @return: java.lang.String
      * @auther: xianzilei
      * @date: 2019/11/24 21:08
      **/
     @GetMapping("/blogs")
-    public String blogs(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC)Pageable pageable, BlogQuery blog, Model model) {
+    public String blogs(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable, BlogQuery blog, Model model) {
         model.addAttribute("types", typeService.listType());
         model.addAttribute("page", blogService.listBlog(pageable, blog));
-        return "admin/blogs";
+        return LIST;
     }
 
     /**
      * 返回片段
      *
      * @param pageable 1
-     * @param blog 2
-     * @param model 3
+     * @param blog     2
+     * @param model    3
      * @return: java.lang.String
      * @auther: xianzilei
      * @date: 2019/11/24 21:26
@@ -64,5 +67,31 @@ public class BlogController {
                          BlogQuery blog, Model model) {
         model.addAttribute("page", blogService.listBlog(pageable, blog));
         return "admin/blogs :: blogList";
+    }
+
+    /**
+     * 功能描述: 跳转博客新增页面
+     *
+     * @param model 1
+     * @return: java.lang.String
+     * @auther: xianzilei
+     * @date: 2019/11/25 8:51
+     **/
+    @GetMapping("/blogs/input")
+    public String input(Model model) {
+        setTypeAndTag(model);
+        model.addAttribute("blog", new Blog());
+        return INPUT;
+    }
+
+    /**
+     * 功能描述: 组装标签和分类信息
+     *
+     * @param model 1
+     * @return: void
+     **/
+    private void setTypeAndTag(Model model) {
+        model.addAttribute("types", typeService.listType());
+        model.addAttribute("tags", tagService.listTag());
     }
 }
