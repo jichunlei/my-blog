9f5751d02b2413f602dcf1f49f0167be3fc766ef
jichunlei <1739575190@qq.com>
2019-12-17 20:22:56 +0800

    初步开始后台用户操作代码编写

---
+++ src/main/java/com/jicl/controller/user/BlogController.java
@@ -0,0 +1,13 @@
+package com.jicl.controller.user;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * TODO
+ *
+ * @author : xianzilei
+ * @date : 2019/12/17 20:22
+ */
+@Controller
+public class BlogController {
+}
