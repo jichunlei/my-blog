a629e67738b7fb456d165411fd1fd07d296a6b9a
xianzilei <1739575190@qq.com>
2020-04-29 21:45:44 +0800

    修复缺陷

--- src/main/java/com/jicl/MyblogApplication.java
+++ src/main/java/com/jicl/MyblogApplication.java
@@ -12,8 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.jicl.mapper")
 @SpringBootApplication
 public class MyblogApplication {
-
     public static void main(String[] args) {
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
         SpringApplication.run(MyblogApplication.class, args);
     }
 
